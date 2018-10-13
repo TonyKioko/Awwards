a0c5b4b43bf944d60839e2cf4b43795ddb931984
TonyKioko <tonykioko384@gmail.com>
2018-10-13 18:52:40 +0300

    added timezone to profile model

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils import timezone
+
 # Create your models here.
 
 class Profile(models.Model):
@@ -12,7 +14,9 @@ class Profile(models.Model):
     # user = models.ForeignKey(User, null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile",primary_key=True)
     contact = models.CharField(max_length=60,blank=True)
-    timestamp = models.DateTimeField(auto_now_add=True,null = True)
+    timestamp = models.DateTimeField(default=timezone.now())
+
+    # timestamp = models.DateTimeField(auto_now_add=True,null = True)
 
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
