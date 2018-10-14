cbf5f03d48a354a0ffdc80d21568bff2f22e5c77
TonyKioko <tonykioko384@gmail.com>
2018-10-14 20:16:21 +0300

    configuring djangorest framework

--- awwards/settings.py
+++ awwards/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'app',
     'bootstrap4',
     'tinymce',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
